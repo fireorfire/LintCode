0b6979a33f362f000d74f58d80b47e8066d35d05
James Lau <liuzijianhuolong@163.com>
2015-05-04 10:09:38 -0700

    segment tree modify

---
+++ segmentTreeModify.java
@@ -0,0 +1,34 @@
+/**
+ * Definition of SegmentTreeNode:
+ * public class SegmentTreeNode {
+ *     public int start, end, max;
+ *     public SegmentTreeNode left, right;
+ *     public SegmentTreeNode(int start, int end, int max) {
+ *         this.start = start;
+ *         this.end = end;
+ *         this.max = max
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     *@param root, index, value: The root of segment tree and 
+     *@ change the node's value with [index, index] to the new given value
+     *@return: void
+     */
+    public void modify(SegmentTreeNode root, int index, int value) {
+        // write your code here
+        if(root == null) return;
+        if(root.start == root.end) {
+            if(root.start == index) {
+                root.max = value;
+            }
+        }else {
+            int mid = (root.start + root.end)/2;
+            if(mid >= index)modify(root.left, index, value);
+            else modify(root.right, index, value);
+            root.max = Math.max(root.left.max, root.right.max);
+        }
+    }
+}
